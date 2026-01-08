1206fc6832fea0369b1ddec8ac8f2a6f739a5b22
Jon <Jon.buesa.41441@ikasle.egibide.org>
2026-01-08 12:43:50 +0100

    v16

---
+++ POO/Ejercicio1/src/Circunferencia.java
@@ -0,0 +1,26 @@
+public class Circunferencia {
+
+
+    public Circunferencia(double radio) {
+
+    }
+
+
+    public static void setRadio(double radio) {
+
+    }
+
+    public static void CalcularLongitud(){
+
+    }
+
+    public static void CalcularArea(){
+
+    }
+
+    public static void CalcularVolumen(){
+
+    }
+
+
+}
---
+++ POO/Ejercicio1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Circunferencia e1 = new Circunferencia(12);
+        Circunferencia e2 = new Circunferencia(5.67);
+        Circunferencia e3 = new Circunferencia(9.3);
+
+    }
+}
