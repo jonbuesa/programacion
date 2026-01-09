29403d090fa7e4423f4963f58a0eca82ed0bfecc
Jon <Jon.buesa.41441@ikasle.egibide.org>
2026-01-09 12:33:43 +0100

    vv

--- POO/Ejercicio1/src/Circunferencia.java
+++ POO/Ejercicio1/src/Circunferencia.java
@@ -1,26 +1,28 @@
 public class Circunferencia {
 
+    private double radio;
 
     public Circunferencia(double radio) {
-
+        this.radio = radio;
     }
 
-
-    public static void setRadio(double radio) {
-
+    public double getRadio() {
+        return radio;
     }
 
-    public static void CalcularLongitud(){
-
+    public void setRadio(double radio) {
+        this.radio = radio;
     }
 
-    public static void CalcularArea(){
-
+    public double calcularCircunferencia(){
+        return 2 * Math.PI * radio;
     }
 
-    public static void CalcularVolumen(){
-
+    public double calcularArea(){
+        return Math.PI * radio * radio;
     }
 
-
+    public double calcularVolume(){
+        return 4 * Math.PI * radio * radio * radio /3;
+    }
 }
--- POO/Ejercicio1/src/Main.java
+++ POO/Ejercicio1/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Circunferencia e1 = new Circunferencia(12);
-        Circunferencia e2 = new Circunferencia(5.67);
-        Circunferencia e3 = new Circunferencia(9.3);
+        Circunferencia c1 = new Circunferencia(12);
+        Circunferencia c2 = new Circunferencia(5.67);
+        Circunferencia c3 = new Circunferencia(9.3);
 
+
+        System.out.println("Radio " + c1.getRadio() + "\n" + "Circunferencia c1: " + c1.calcularCircunferencia()  + "\n" +
+                " Area: " + c1.calcularArea() + "\n" +
+                " Volumen: " + c1.calcularVolume());
     }
 }
