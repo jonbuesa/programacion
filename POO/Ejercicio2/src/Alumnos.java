37e8e857af2b8062830dee3b136c23ee0fb623f7
Jon <Jon.buesa.41441@ikasle.egibide.org>
2026-01-09 13:39:02 +0100

    vv

---
+++ POO/Ejercicio2/src/Alumnos.java
@@ -0,0 +1,48 @@
+class Datos_Alumnos {
+    private String  codigo;
+    private String nombre;
+    private String domicilio;
+    private String telefono;
+
+    public Datos_Alumnos(int i, String juanPerez, String domicilio, String telefono) {
+    }
+
+    public Datos_Alumnos(String codigo, String nombre, String domicilio, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
--- POO/Ejercicio2/src/Main.java
+++ POO/Ejercicio2/src/Main.java
@@ -1,8 +1,53 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("hola mundo");
-        System.out.println("j");
+        String  codigo;
+        String  nombre;
+        String  domicilio;
+        String  telefono;
+
+
+        System.out.println("Cuantos alumnos desea ingresar? ");
+        int cantidadAlumnos = Integer.parseInt(sc.nextLine());
+        Datos_Alumnos[] listaAlumnos = new Datos_Alumnos[cantidadAlumnos];
+
+        for (int i = 0; i < cantidadAlumnos; i++) {
+            System.out.println("Ingrese el codigo del alumno " + (i + 1) + ": ");
+            codigo = sc.nextLine();
+            System.out.println("Ingrese el nombre del alumno " + (i + 1) + ": ");
+            nombre = sc.nextLine();
+            System.out.println("Ingrese el domicilio del alumno " + (i + 1) + ": ");
+            domicilio = sc.nextLine();
+            System.out.println("Ingrese el telefono del alumno " + (i + 1) + ": ");
+            telefono = sc.nextLine();
+
+            listaAlumnos[i] = new Datos_Alumnos(codigo, nombre, domicilio, telefono);
+        }
+
+
+        System.out.println("Buscar alumno por codigo: ");
+        String codigoBuscar = sc.nextLine();
+
+        for (int i = 0; i < cantidadAlumnos; i++) {
+            if (listaAlumnos[i].getCodigo().equals(codigoBuscar)) {
+                System.out.println("Alumno encontrado:");
+                System.out.println("Codigo: " + listaAlumnos[i].getCodigo());
+                System.out.println("Nombre: " + listaAlumnos[i].getNombre());
+                System.out.println("Domicilio: " + listaAlumnos[i].getDomicilio());
+                System.out.println("Telefono: " + listaAlumnos[i].getTelefono());
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }
