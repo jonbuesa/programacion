2e8685c2d0e84285cd062a5a99e879cdce6a9734
Jon <Jon.buesa.41441@ikasle.egibide.org>
2026-01-12 11:57:28 +0100

    j3

--- POO/Ejercicio3/src/Personas.java
+++ POO/Ejercicio3/src/Personas.java
@@ -1,34 +1,22 @@
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.Scanner;
+class Persona {
 
-public class Personas {
     private String nombre;
+    private String direccion;
+    private String codigoPostal;
+    private String ciudad;
     private int dNacimiento;
     private int mNacimiento;
     private int aNacimiento;
-    private String direccion;
-    private int codigoPostal;
-    private String ciudad;
 
-    public Personas(String ciudad, int codigoPostal, String direccion, int aNacimiento, int mNacimiento, int dNacimiento, String nombre) {
-        this.ciudad = ciudad;
-        this.codigoPostal = codigoPostal;
+    public Persona(String nombre, String direccion, String codigoPostal, String ciudad, int dNacimiento, int aNacimiento, int mNacimiento) {
+        this.nombre = nombre;
         this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        this.ciudad = ciudad;
+        this.dNacimiento = dNacimiento;
         this.aNacimiento = aNacimiento;
         this.mNacimiento = mNacimiento;
-        this.dNacimiento = dNacimiento;
-        this.nombre = nombre;
     }
-
-    public LocalDate getFechaNacimiento(){
-        return LocalDate.of(dNacimiento,mNacimiento,aNacimiento);
-    }
-
-    public int getEdad(){
-        return Period.between(getFechaNacimiento(),LocalDate.now()).getYears();
-    }
-
 
     public String getNombre() {
         return nombre;
@@ -36,6 +24,30 @@ public class Personas {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
     }
 
     public int getdNacimiento() {
@@ -60,42 +72,5 @@ public class Personas {
 
     public void setaNacimiento(int aNacimiento) {
         this.aNacimiento = aNacimiento;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public int getCodigoPostal() {
-        return codigoPostal;
-    }
-
-    public void setCodigoPostal(int codigoPostal) {
-        this.codigoPostal = codigoPostal;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
-    @Override
-    public String toString() {
-        return "Personas{" +
-                "nombre='" + nombre + '\'' +
-                ", dNacimiento=" + dNacimiento +
-                ", mNacimiento=" + mNacimiento +
-                ", aNacimiento=" + aNacimiento +
-                ", direccion='" + direccion + '\'' +
-                ", codigoPostal=" + codigoPostal +
-                ", ciudad='" + ciudad + '\'' +
-                '}';
     }
 }
