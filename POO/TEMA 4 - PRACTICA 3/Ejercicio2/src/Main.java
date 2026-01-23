bfc9cbd19ce9084abebbf352c2fb611f01662825
Jon <Jon.buesa.41441@ikasle.egibide.org>
2026-01-23 14:07:38 +0100

    jon

--- POO/TEMA 4 - PRACTICA 3/Ejercicio2/src/Main.java
+++ POO/TEMA 4 - PRACTICA 3/Ejercicio2/src/Main.java
@@ -11,11 +11,10 @@ public class Main {
 
     public static void menu(){
         System.out.println("que opcion quieres? \n"
-                        + "1. Mostrar numero de clientes \n"
+                        + "1. Mostrar numero de clientes con un animal concreto \n"
                         + "2. Datos personales de un cliente \n"
                         + "3. Datos del veterinario \n"
                         + "4. Datos de la mascota de un cliente \n"
                         + "5. Datos de la mascota de un veterinario");
-
     }
 }
